f089c5c16e598231e60c2dd0e9baa7669f0d4f81
Marian Anghel <66934336+maang-max@users.noreply.github.com>
2025-04-10 16:29:00 +0300

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,1 +1,21 @@
+# Fetch the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"] # Amazon-owned AMIs
+}
+
+# Fetch the default VPC (if using an existing VPC instead of creating a new one)
+data "aws_vpc" "default_vpc" {
+  default = true
+}
