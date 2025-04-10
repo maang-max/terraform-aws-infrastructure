6f3cd812cfdf024ffeadc1dff609844cff197a57
Marian Anghel <66934336+maang-max@users.noreply.github.com>
2025-04-10 17:50:53 +0300

    Update main.tf
    output alb_dns_name

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ module "security_groups" {
 module "load_balancer" {
   source = "./load_balancer"
 }
+output "alb_dns_name" {
+  description = "The DNS name of the Application Load Balancer."
+  value       = module.load_balancer.alb_dns_name
+}
 
 # EC2 Instances and Auto Scaling Group
 module "ec2" {
